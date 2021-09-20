70cafa30e9634059fb69a0720222899db1d63422
dj-mc <49037253+dj-mc@users.noreply.github.com>
2021-09-19 19:51:56 -0500

    Add sh script for generating env file, add style

---
+++ .env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# .env defaults:
+#   FLASK_APP=.
+#   FLASK_ENV=development
+
+
+if [ ! -f ".env" ]; then
+  touch .env
+  printf "FLASK_APP=.\nFLASK_ENV=development\n" >> .env
+  echo "Made default .env file"
+else
+  echo ".env already exists"
+fi
